30602ac4df301ee64982aaca24c0fbb7122471ec
Mohammad Mehrnia <qermezkon@gmail.com>
2020-10-31 11:15:43 +0330

    Initialize

---
+++ very_easy_test_app/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace very_easy_test_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
